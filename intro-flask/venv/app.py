917786e70667babd37ef6a03dc94a2c7f9bbb9f5
MpereiraWare <mpereira@warehousing.cl>
2022-12-16 16:28:49 -0300

    more changes using http commands

--- intro-flask/venv/app.py
+++ intro-flask/venv/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index() :
     return 'hola mundo'
 
-@app.route('/lala')
-def lala() :
-    return 'lala'
+#GET, POST, PUT, PATCH, DELETE
+@app.route('/post/<post_id>', methods=['GET', 'POST'])
+def lala(post_id) :
+    if request.method== 'GET':
+        return 'El id del post es: ' + post_id 
+    else:
+        return 'Este es otro metodo y no GET'
+
+#curl.exe -d "llave1=dato1&llave2=dato2" -X POST http://localhost:5000/lele
+@app.route('/lele', methods=['POST'])
+def lele() :
+    print (request.form)
+    print (request.form['llave1'])
+    print (request.form['llave2'])
+    return 'lele'
