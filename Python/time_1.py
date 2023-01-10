1c3eca986bfcbf6b21a08582583985a2cb81e0d5
MpereiraWare <mpereira@warehousing.cl>
2023-01-10 17:24:40 -0300

    Create time_1.py

---
+++ Python/time_1.py
@@ -0,0 +1,12 @@
+import time
+from datetime import datetime
+
+cur_time = time.time()
+print(cur_time)
+
+time_struct = time.localtime(cur_time)
+print(time_struct)
+
+print(time.asctime(time_struct))
+
+print("Good Night")
